49ba9ae98ed80d5de64afef5d126895accd74dc8
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-25 00:02:55 +0300

    finish the login route

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const logger = require("./logger/logger");
+const login = require("./app/login");
+
 const app = express();
 const port = 3000;
 
-app.get("/login", (request, response) => {
-    logger.info("Login Page Accessed");
-    response.send("Hello World!");
+app.use(express.json());
+
+app.post("/login", async (request, response) => {
+  logger.info("Login Page Accessed");
+
+  try {
+    const username = request.body.username;
+    const password = request.body.password;
+
+    // logging the username
+    logger.info(`Username: ${username}`);
+
+    const credentials = {
+      username: username,
+      password: password,
+    };
+
+    const isValidated = await login(credentials);
+
+    // logging the validation result
+    logger.info(`Is Validated: ${isValidated}`);
+
+    response.json(isValidated);
+  } catch (error) {
+    logger.error(error);
+    response.json(error);
+  }
 });
 
 //Initialises the express server on the port 3000
