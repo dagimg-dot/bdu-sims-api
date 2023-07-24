6f67121e6c61a372416a8ded35c8ba2550ff147b
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-24 17:59:55 +0300

    integrate pino with the server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const logger = require("./logger/logger");
 const app = express();
 const port = 3000;
 
 app.get("/login", (request, response) => {
+    logger.info("Login Page Accessed");
     response.send("Hello World!");
 });
 
 //Initialises the express server on the port 3000
-app.listen(port, () => console.log(`Server listening on port ${port}!`));
+app.listen(port, () => logger.info(`Server listening on port ${port}!`));
