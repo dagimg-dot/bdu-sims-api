33e0869718dff467531c752ccda9c0a77ae892d4
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-26 01:25:18 +0300

    make the routes their own files

--- server.js
+++ server.js
@@ -1,83 +1,19 @@
 const express = require("express");
-const dotenv = require("dotenv");
-const jwt = require("jsonwebtoken");
-dotenv.config();
 
-const logger = require("./logger/logger");
 const login = require("./auth/login");
-const getIPAddress = require("./utils/ipUtils");
+const logger = require("./logger/logger");
+const courses = require("./api/courses");
 const authenticationMiddleware = require("./middleware/authMiddleware");
 
 const app = express();
 const port = process.env.EXPRESS_PORT;
-const secret_key = process.env.JWT_SECRET;
-const expiresIn = process.env.JWT_EXPIRES_IN;
 const ipAddress = "192.168.1.4";
 
 app.use(express.json());
 
-app.post("/auth/login", async (request, response) => {
-  logger.info(`Login Page requested from IP Address: ${getIPAddress(request)}`);
+app.post("/auth/login", login);
 
-  try {
-    const username = request.body.username;
-    const password = request.body.password;
-
-    // logging the username
-    logger.info(`Username: ${username}`);
-
-    const credentials = {
-      username: username,
-      password: password,
-    };
-
-    const isValidated = await login(credentials);
-
-    // logging the validation result
-    logger.info(`Is Validated: ${isValidated}`);
-
-    if (isValidated) {
-      const tokenContent = {
-        username: username,
-        iat: Date.now() / 1000,
-      };
-      const token = jwt.sign(tokenContent, secret_key, {expiresIn: expiresIn});
-
-      response.header("Authorization", token);
-
-      response.status(200).json({ status: "success" });
-    } else {
-      response
-        .status(401)
-        .json({ status: "failed", message: "Invalid Credentials" });
-    }
-  } catch (error) {
-    logger.error(error);
-    response.json(error);
-  }
-});
-
-app.get("/api/courses", authenticationMiddleware, (request, response) => {
-    logger.info(
-        `Courses Page requested from IP Address: ${getIPAddress(request)}`
-    );
-    response.json({
-        courses: [
-        {
-            id: 1,
-            name: "Software Engineering",
-        },
-        {
-            id: 2,
-            name: "Computer Science",
-        },
-        {
-            id: 3,
-            name: "Information Technology",
-        },
-        ],
-    });
-});
+app.get("/api/courses", authenticationMiddleware, courses);
 
 //Initialises the express server on the port 3000
 app.listen(port, ipAddress, () =>
