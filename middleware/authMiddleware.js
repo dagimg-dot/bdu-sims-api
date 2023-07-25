5b540771e650f95ad1f92a5b9704b966fb14c2f0
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-26 00:37:34 +0300

    create authmiddlware to verify logged in users

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const secretKey = process.env.JWT_SECRET;
+
+function authenticationMiddleware(req, res, next) {
+  const auth = req.header('Authorization');
+  const token = auth && auth.split(' ')[1];
+  console.log(token);
+
+  if (!token) {
+    return res.status(401).json({ message: 'Authentication token not provided.' });
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, secretKey);
+    req.user = decodedToken; // Store the decoded token data in the request object for future use
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid or expired authentication token.' });
+  }
+}
+
+module.exports = authenticationMiddleware;
