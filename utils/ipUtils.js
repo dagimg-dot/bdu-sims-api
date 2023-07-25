b16af4d62286988c969d0eb02a010384ae9c84a1
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-26 01:49:45 +0300

    remove ip package

--- utils/ipUtils.js
+++ utils/ipUtils.js
@@ -1,11 +1,9 @@
-// ipUtils.js
 const getIPAddress = (req) => {
   // Extract the IP address from the request object
   const ipAddress =
     req.headers["x-forwarded-for"] ||
-    req.connection.remoteAddress 
+    req.connection.remoteAddress;
 
-  // return ipAddress.replace('::ffff:', '');
   return ipAddress;
 };
 
