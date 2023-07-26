4a0c54932bef97b576bc870c0276d3212a969511
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-26 16:14:06 +0300

    use the browserpool to get browser instance

--- scraper/s_login.js
+++ scraper/s_login.js
@@ -1,34 +1,31 @@
-const puppeteer = require("puppeteer");
-// const Browser = require("./browser");
+const browserPool = require("../utils/browser");
 
 const login = async (credentials) => {
-  // const browser = await Browser.getInstance();
   try {
-    const browser = await puppeteer.launch({ headless: "new" });
+    const browser = await browserPool.createBrowserInstance(credentials.username);
+
     const page = await browser.newPage();
-  
+
     await page.goto("https://studentinfo.bdu.edu.et/login.aspx");
-  
+
     const username = await page.$("#dnn_ctr_login_login_dnn_txtusername");
     await username.type(credentials.username);
-  
+
     const password = await page.$("#dnn_ctr_login_login_dnn_txtpassword");
     await password.type(credentials.password);
-  
+
     const login = await page.$("#dnn_ctr_login_login_dnn_cmdlogin");
     await login.click();
-  
+
     await page.waitForNavigation();
- 
+
     if ((await page.$("#dnn_dnnuser_cmdregister")) != null) {
-      // await browser.close();
       return true;
     } else {
-      // await browser.close();
+      await browser.close();
       return false;
     }
   } catch (error) {
-    // await browser.close();
     return error;
   }
 };
