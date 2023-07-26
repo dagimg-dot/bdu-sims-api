162a5560ea818147627ace9d0d80f15508a8a8f2
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-26 14:14:13 +0300

    better error handling

--- scraper/s_login.js
+++ scraper/s_login.js
@@ -3,28 +3,33 @@ const puppeteer = require("puppeteer");
 
 const login = async (credentials) => {
   // const browser = await Browser.getInstance();
-  const browser = await puppeteer.launch({ headless: "new" });
-  const page = await browser.newPage();
-
-  await page.goto("https://studentinfo.bdu.edu.et/login.aspx");
-
-  const username = await page.$("#dnn_ctr_Login_Login_DNN_txtUsername");
-  await username.type(credentials.username);
-
-  const password = await page.$("#dnn_ctr_Login_Login_DNN_txtPassword");
-  await password.type(credentials.password);
-
-  const login = await page.$("#dnn_ctr_Login_Login_DNN_cmdLogin");
-  await login.click();
-
-  await page.waitForNavigation();
-
-  if ((await page.$("#dnn_dnnUSER_cmdRegister")) != null) {
+  try {
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
+  
+    await page.goto("https://studentinfo.bdu.edu.et/login.aspx");
+  
+    const username = await page.$("#dnn_ctr_login_login_dnn_txtusername");
+    await username.type(credentials.username);
+  
+    const password = await page.$("#dnn_ctr_login_login_dnn_txtpassword");
+    await password.type(credentials.password);
+  
+    const login = await page.$("#dnn_ctr_login_login_dnn_cmdlogin");
+    await login.click();
+  
+    await page.waitForNavigation();
+ 
+    if ((await page.$("#dnn_dnnuser_cmdregister")) != null) {
+      // await browser.close();
+      return true;
+    } else {
+      // await browser.close();
+      return false;
+    }
+  } catch (error) {
     // await browser.close();
-    return true;
-  } else {
-    // await browser.close();
-    return false;
+    return error;
   }
 };
 
