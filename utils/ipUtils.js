07b5c276403f7a6a3a2d61114b166364ca2814ad
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-26 14:13:59 +0300

    add method to get the machine ip address

--- utils/ipUtils.js
+++ utils/ipUtils.js
@@ -1,10 +1,30 @@
-const getIPAddress = (req) => {
+const os = require("os");
+
+const getClientIPAddress = (req) => {
   // Extract the IP address from the request object
   const ipAddress =
-    req.headers["x-forwarded-for"] ||
-    req.connection.remoteAddress;
+    req.headers["x-forwarded-for"] || req.connection.remoteAddress;
 
   return ipAddress;
 };
 
-module.exports = getIPAddress;
+const getServerIPAddress = () => {
+  const networkInterfaces = os.networkInterfaces();
+  let ipAddress = "127.0.0.1"; // Default to localhost if IP address is not found
+
+  // Loop through all the network interfaces to find a non-internal IPv4 address
+  for (const interfaceName in networkInterfaces) {
+    const interfaces = networkInterfaces[interfaceName];
+    for (const iface of interfaces) {
+      if (!iface.internal && iface.family === "IPv4") {
+        ipAddress = iface.address;
+        break;
+      }
+    }
+  }
+
+  return ipAddress;
+};
+
+module.exports = getClientIPAddress;
+module.exports = getServerIPAddress;
