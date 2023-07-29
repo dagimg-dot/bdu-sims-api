f22b07850507ade45c4817fcb9f7bad3557b36fa
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-29 23:58:45 +0300

    change name of scraper funtion for clarity

--- api/status_general.js
+++ api/status_general.js
@@ -1,13 +1,13 @@
 const logger = require("../logger/logger");
 const getClientIPAddress = require("../utils/ipUtils");
-const s_status = require("../scraper/s_status_general")
+const s_status_general = require("../scraper/s_status_general")
 
 const status = async (request, response) => {
   logger.info(
     `Status Page requested from IP Address: ${getClientIPAddress(request)}`
   );
 
-  const result = await s_status(request);
+  const result = await s_status_general(request);
   if (result !== null) {
     response.status(200).json({
       status: result,
