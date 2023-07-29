ecc98a795934e3a4ce6539d7a07bbadbdeb73a55
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-30 02:56:10 +0300

    better error handling

--- scraper/s_status_general.js
+++ scraper/s_status_general.js
@@ -13,33 +13,37 @@ const status = async (req) => {
 
     const rows = await page.evaluate(() => {
       let count = 0;
-      const rowElements = document.querySelectorAll(
-        "#dnn_ctr397_ViewMyStatus_reportviewer11_grid2_ob_grid2BodyContainer > div.ob_gBICont > table > tbody > tr"
-      );
-      const rowsData = Array.from(rowElements).map((rowElement) => {
-        const tds = rowElement.querySelectorAll("td");
-        if (count % 2 == 0) {
-          count++;
-          return {
-            AcYear: tds[2].querySelector("div > div").innerText,
-            Year: tds[3].querySelector("div > div").innerText,
-            Semester: tds[4].querySelector("div > div").innerText,
-            RegistrationDate: tds[5].querySelector("div > div").innerText,
-            RegType: tds[7].querySelector("div > div").innerText,
-            SGPA: tds[9].querySelector("div > div").innerText,
-            CGPA: tds[10].querySelector("div > div").innerText,
-            PrevStatus: tds[11].querySelector("div > div").innerText,
-            FinalStatus: tds[12].querySelector("div > div").innerText,
-          };
-        } else {
-          count++;
-          return null;
-        }
-      });
-      return rowsData;
+      try {
+        const rowElements = document.querySelectorAll(
+          "#dnn_ctr397_ViewMyStatus_reportviewer11_grid2_ob_grid2BodyContainer > div.ob_gBICont > table > tbody > tr"
+        );
+        const rowsData = Array.from(rowElements).map((rowElement) => {
+          const tds = rowElement.querySelectorAll("td");
+          if (count % 2 == 0) {
+            count++;
+            return {
+              AcYear: tds[2].querySelector("div > div").innerText,
+              Year: tds[3].querySelector("div > div").innerText,
+              Semester: tds[4].querySelector("div > div").innerText,
+              RegistrationDate: tds[5].querySelector("div > div").innerText,
+              RegType: tds[7].querySelector("div > div").innerText,
+              SGPA: tds[9].querySelector("div > div").innerText,
+              CGPA: tds[10].querySelector("div > div").innerText,
+              PrevStatus: tds[11].querySelector("div > div").innerText,
+              FinalStatus: tds[12].querySelector("div > div").innerText,
+            };
+          } else {
+            count++;
+            return "detailGrade";
+          }
+        });
+        return rowsData;
+      } catch (error) {
+        return null;
+      }
     });
-    
-    const filteredRows = rows.filter((row) => row != null);
+
+    const filteredRows = rows.filter((row) => row != "detailGrade");
     return filteredRows;
   } else {
     return null;
