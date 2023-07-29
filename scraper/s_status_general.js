2379a86fdce534058d1bc8bca9417ca910ffbff3
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-29 23:21:45 +0300

    finish the general status scraper

---
+++ scraper/s_status_general.js
@@ -0,0 +1,49 @@
+const browserPool = require("../utils/browser");
+const jwt = require("jsonwebtoken");
+
+const status = async (req) => {
+  const token = req.headers.authorization.split(" ")[1];
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const username = decoded.username;
+
+  const browser = await browserPool.getBrowserInstance(username);
+  if (browser != null) {
+    const page = await browser.newPage();
+    await page.goto("https://studentinfo.bdu.edu.et/MyStatus.aspx");
+
+    const rows = await page.evaluate(() => {
+      let count = 0;
+      const rowElements = document.querySelectorAll(
+        "#dnn_ctr397_ViewMyStatus_reportviewer11_grid2_ob_grid2BodyContainer > div.ob_gBICont > table > tbody > tr"
+      );
+      const rowsData = Array.from(rowElements).map((rowElement) => {
+        const tds = rowElement.querySelectorAll("td");
+        if (count % 2 == 0) {
+          count++;
+          return {
+            AcYear: tds[2].querySelector("div > div").innerText,
+            Year: tds[3].querySelector("div > div").innerText,
+            Semester: tds[4].querySelector("div > div").innerText,
+            RegistrationDate: tds[5].querySelector("div > div").innerText,
+            RegType: tds[7].querySelector("div > div").innerText,
+            SGPA: tds[9].querySelector("div > div").innerText,
+            CGPA: tds[10].querySelector("div > div").innerText,
+            PrevStatus: tds[11].querySelector("div > div").innerText,
+            FinalStatus: tds[12].querySelector("div > div").innerText,
+          };
+        } else {
+          count++;
+          return null;
+        }
+      });
+      return rowsData;
+    });
+    
+    const filteredRows = rows.filter((row) => row != null);
+    return filteredRows;
+  } else {
+    return null;
+  }
+};
+
+module.exports = status;
