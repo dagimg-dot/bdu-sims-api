234f1c44a882fd27f65af807274a4fac0ca5f839
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-29 23:21:31 +0300

    finish the general status endpoint

---
+++ api/status_general.js
@@ -0,0 +1,26 @@
+const logger = require("../logger/logger");
+const getClientIPAddress = require("../utils/ipUtils");
+const s_status = require("../scraper/s_status_general")
+
+const status = async (request, response) => {
+  logger.info(
+    `Status Page requested from IP Address: ${getClientIPAddress(request)}`
+  );
+
+  const result = await s_status(request);
+  if (result !== null) {
+    response.status(200).json({
+      status: result,
+    });
+  } else if (result === null) {
+    response.status(401).json({
+      message: "Unauthorized",
+    });
+  } else {
+    response.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+module.exports = status;
