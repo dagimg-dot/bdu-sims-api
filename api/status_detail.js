9b9f2ec0388b4d213053b525acc749b70b3f3235
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-30 03:28:14 +0300

    finish the status detail endpoint

---
+++ api/status_detail.js
@@ -0,0 +1,35 @@
+const logger = require("../logger/logger");
+const getClientIPAddress = require("../utils/ipUtils");
+const s_status_detail = require("../scraper/s_status_detail");
+
+const courses = async (request, response) => {
+  logger.info(
+    `Status Page requested from IP Address: ${getClientIPAddress(request)}`
+  );
+
+  const year = request.params.year;
+  const semester = request.params.semester;
+
+  if (isNaN(year) || isNaN(semester) || semester < 1 || semester >= 3) {
+    response.status(400).json({
+      message: "Bad Request",
+    });
+  } else {
+    const result = await s_status_detail(request);
+    if (result !== null) {
+      response.status(200).json({
+        courses: result,
+      });
+    } else if (result === null) {
+      response.status(401).json({
+        message: "Unauthorized",
+      });
+    } else {
+      response.status(500).json({
+        message: "Internal Server Error",
+      });
+    }
+  }
+};
+
+module.exports = courses;
