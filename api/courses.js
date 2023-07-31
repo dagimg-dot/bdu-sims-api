d1cd16b974620a5313f2a2b5c2de8dc396ba9d42
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-31 17:43:16 +0300

    add way to respond from cache if request is > 1

--- api/courses.js
+++ api/courses.js
@@ -1,25 +1,35 @@
 const logger = require("../logger/logger");
 const getClientIPAddress = require("../utils/ipUtils");
 const s_courses = require("../scraper/s_courses");
+const User = require("../memory_db/user");
+const getUsername = require("../utils/usernameHandler");
 
 const courses = async (request, response) => {
   logger.info(
     `Courses Page requested from IP Address: ${getClientIPAddress(request)}`
   );
 
-  const result = await s_courses(request);
-  if (result !== null) {
+  const user = User.getUser(getUsername(request));
+  if (user.getCourses() !== null) {
     response.status(200).json({
-      courses: result,
-    });
-  } else if (result === null) {
-    response.status(401).json({
-      message: "Unauthorized",
+      courses: user.getCourses(),
     });
   } else {
-    response.status(500).json({
-      message: "Internal Server Error",
-    });
+    const result = await s_courses(request);
+    if (result !== null) {
+      user.setCourses(result);
+      response.status(200).json({
+        courses: result,
+      });
+    } else if (result === null) {
+      response.status(401).json({
+        message: "Unauthorized",
+      });
+    } else {
+      response.status(500).json({
+        message: "Internal Server Error",
+      });
+    }
   }
 };
 
