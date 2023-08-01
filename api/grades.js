6e5dfef2d6903bad9232c4e24abb8543f63d403e
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-01 17:04:40 +0300

    implement caching

--- api/grades.js
+++ api/grades.js
@@ -1,25 +1,37 @@
 const logger = require("../logger/logger");
 const getClientIPAddress = require("../utils/ipUtils");
 const s_grades = require("../scraper/s_grades");
+const User = require("../memory_db/user");
+const getUsername = require("../utils/usernameHandler");
 
 const courses = async (request, response) => {
   logger.info(
     `Grades Page requested from IP Address: ${getClientIPAddress(request)}`
   );
 
-  const result = await s_grades(request);
-  if (result !== null) {
+  const { username, isExpired } = getUsername(request);
+  const user = User.getUser(username);
+
+  if (user.getGrades() !== null) {
     response.status(200).json({
-      grades: result,
-    });
-  } else if (result === null) {
-    response.status(401).json({
-      message: "Unauthorized",
+      grades: user.getGrades(),
     });
   } else {
-    response.status(500).json({
-      message: "Internal Server Error",
-    });
+    const result = await s_grades(request);
+    if (result !== null) {
+      user.setGrades(result);
+      response.status(200).json({
+        grades: result,
+      });
+    } else if (result === null) {
+      response.status(401).json({
+        message: "Unauthorized",
+      });
+    } else {
+      response.status(500).json({
+        message: "Internal Server Error",
+      });
+    }
   }
 };
 
--- api/info.js
+++ api/info.js
@@ -1,25 +1,37 @@
 const logger = require("../logger/logger");
 const getClientIPAddress = require("../utils/ipUtils");
 const s_info = require("../scraper/s_info");
+const User = require("../memory_db/user");
+const getUsername = require("../utils/usernameHandler");
 
 const info = async (request, response) => {
   logger.info(
     `Info Page requested from IP Address: ${getClientIPAddress(request)}`
   );
 
-  const result = await s_info(request);
-  if (result !== null) {
+  const { username, isExpired } = getUsername(request);
+  const user = User.getUser(username);
+
+  if (user.getInfo() !== null) {
     response.status(200).json({
-      info: result,
-    });
-  } else if (result === null) {
-    response.status(401).json({
-      message: "Unauthorized",
+      info: user.getInfo(),
     });
   } else {
-    response.status(500).json({
-      message: "Internal Server Error",
-    });
+    const result = await s_info(request);
+    if (result !== null) {
+      user.setInfo(result);
+      response.status(200).json({
+        info: result,
+      });
+    } else if (result === null) {
+      response.status(401).json({
+        message: "Unauthorized",
+      });
+    } else {
+      response.status(500).json({
+        message: "Internal Server Error",
+      });
+    }
   }
 };
 
