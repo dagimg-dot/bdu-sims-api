044b9296fd2386df2ed8b1957d11556d20ba6a58
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-02 19:06:02 +0300

    better error message for the log

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -3,12 +3,15 @@ const User = require('../memory_db/user');
 
 const handleError = (error, username) => {
     const user = User.getUser(username);
-    if (error.message.includes("net")) {
-        logger.info(`Something is wrong in ${user.ipaddress} connection`);
-    } else if(error.message.includes("Navigation")) {
+    if (error.message.includes("ERR_NAME_NOT_RESOLVED")) {
+        logger.info(`Unable to resolve the hostname.`);
+    } else if(error.message.includes("ERR_INTERNET_DISCONNECTED")) {
+        logger.info(`Internet connection not available.`)
+    }   
+    else if(error.message.includes("Navigation")) {
         logger.info(`Request from ${user.ipaddress} is timed out`);
         user.setRequested(null);
-    }
+    } 
 }
 
 module.exports = handleError;
