093b2324b39d6f0c01a2e2c71d0ba3b4d7f96b0c
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-02 11:38:11 +0300

    create enums for clean code

---
+++ utils/types.js
@@ -0,0 +1,9 @@
+const Pages = {
+    INFO: 'info',
+    COURSES: 'courses',
+    GRADES: 'grades',
+    GENERAL_STATUS: 'generalStatus',
+    DETAIL_STATUS: 'detailStatus'
+}
+
+module.exports = Pages;
