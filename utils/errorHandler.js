fbe004c259500a85049add95042f66a4b21a3109
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-02 11:40:47 +0300

    handle the request timout efficiently

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,8 +1,13 @@
 const logger = require('../logger/logger');
+const User = require('../memory_db/user');
 
-const handleError = (error) => {
+const handleError = (error, username) => {
+    const user = User.getUser(username);
     if (error.message.includes("net")) {
-        logger.info(`Something is wrong in ${credentials.username} connection`);
+        logger.info(`Something is wrong in ${user.ipaddress} connection`);
+    } else if(error.message.includes("Navigation")) {
+        logger.info(`Request from ${user.ipaddress} is timed out`);
+        user.setRequested(null);
     }
 }
 
