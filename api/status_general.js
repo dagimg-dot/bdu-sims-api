b5dba3056c7425b8438abc7108e403fb63629006
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-10 14:22:24 +0300

    implement caching in status endpoint

--- api/status_general.js
+++ api/status_general.js
@@ -1,25 +1,37 @@
 const logger = require("../logger/logger");
 const getClientIPAddress = require("../utils/ipUtils");
-const s_status_general = require("../scraper/s_status_general")
+const s_status_general = require("../scraper/s_status_general");
+const User = require("../memory_db/user");
+const getUsername = require("../utils/usernameHandler");
 
 const status = async (request, response) => {
   logger.info(
     `Status Page requested from IP Address: ${getClientIPAddress(request)}`
   );
 
-  const result = await s_status_general(request);
-  if (result !== null) {
+  const { username, isExpired } = getUsername(request);
+  const user = User.getUser(username);
+
+  if (user.getGrades() !== null) {
     response.status(200).json({
-      status: result,
-    });
-  } else if (result === null) {
-    response.status(401).json({
-      message: "Unauthorized",
+      status: user.getGeneralStatus(),
     });
   } else {
-    response.status(500).json({
-      message: "Internal Server Error",
-    });
+    const result = await s_status_general(request);
+    if (result !== null) {
+      user.setGeneralStatus(result);
+      response.status(200).json({
+        status: result,
+      });
+    } else if (result === null) {
+      response.status(401).json({
+        message: "Unauthorized",
+      });
+    } else {
+      response.status(500).json({
+        message: "Internal Server Error",
+      });
+    }
   }
 };
 
