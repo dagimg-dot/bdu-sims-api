0dacf5bce875773d7c3d785c80ddd4b1e0c9bd26
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-10 14:01:07 +0300

    fix request timeout error in grades endpoint

--- api/grades.js
+++ api/grades.js
@@ -3,6 +3,7 @@ const getClientIPAddress = require("../utils/ipUtils");
 const s_grades = require("../scraper/s_grades");
 const User = require("../memory_db/user");
 const getUsername = require("../utils/usernameHandler");
+const Pages = require("../utils/types");
 
 const courses = async (request, response) => {
   logger.info(
@@ -11,6 +12,7 @@ const courses = async (request, response) => {
 
   const { username, isExpired } = getUsername(request);
   const user = User.getUser(username);
+  user.setRequested(Pages.GRADES);
 
   if (user.getGrades() !== null) {
     response.status(200).json({
@@ -18,19 +20,21 @@ const courses = async (request, response) => {
     });
   } else {
     const result = await s_grades(request);
-    if (result !== null) {
-      user.setGrades(result);
-      response.status(200).json({
-        grades: result,
-      });
-    } else if (result === null) {
-      response.status(401).json({
-        message: "Unauthorized",
-      });
-    } else {
-      response.status(500).json({
-        message: "Internal Server Error",
-      });
+    if (!response.headersSent) {
+      if (result !== null) {
+        user.setGrades(result);
+        response.status(200).json({
+          grades: result,
+        });
+      } else if (result === null) {
+        response.status(401).json({
+          message: "Unauthorized",
+        });
+      } else {
+        response.status(500).json({
+          message: "Internal Server Error",
+        });
+      }
     }
   }
 };
