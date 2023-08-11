7267df2c832c3f22bb78b10d358176ea19a9b3ff
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-11 14:14:48 +0300

    better error handling in timeout and other errors in info scraper

--- scraper/s_info.js
+++ scraper/s_info.js
@@ -1,5 +1,8 @@
 const browserPool = require("../utils/browser");
 const jwt = require("jsonwebtoken");
+const handleError = require("../utils/errorHandler");
+const User = require("../memory_db/user");
+const Pages = require("../utils/types");
 
 const info = async (request) => {
   const token = request.headers.authorization.split(" ")[1];
@@ -9,26 +12,35 @@ const info = async (request) => {
   const browser = await browserPool.getBrowserInstance(username);
   if (browser != null) {
     const page = await browser.newPage();
-    await page.goto("https://studentinfo.bdu.edu.et");
-    const rows = await page.evaluate(() => {
-      const rowElements = document.querySelectorAll(
-        "#dnn_ctr409_HtmlModule_lblContent > span > table > tbody > tr"
-      );
-      const rowsData = Array.from(rowElements)
-        .slice(1)
-        .map((rowElement) => {
-          const tds = rowElement.querySelectorAll("td");
-          return {
-            id: tds[0].querySelector("span").innerText.split(".")[0],
-            contactName: tds[1].querySelector("span").innerText,
-            position: tds[2].querySelector("span").innerText,
-            college: tds[3].querySelector("span").innerText,
-            contactPhone: tds[4].querySelector("span").innerText,
-          };
-        });
-      return rowsData;
-    });
-    return rows;
+
+    const user = User.getUser(username);
+
+    user.pages[Pages.INFO].value = page;
+
+    try {
+      await page.goto("https://studentinfo.bdu.edu.et");
+      const rows = await page.evaluate(() => {
+        const rowElements = document.querySelectorAll(
+          "#dnn_ctr409_HtmlModule_lblContent > span > table > tbody > tr"
+        );
+        const rowsData = Array.from(rowElements)
+          .slice(1)
+          .map((rowElement) => {
+            const tds = rowElement.querySelectorAll("td");
+            return {
+              id: tds[0].querySelector("span").innerText.split(".")[0],
+              contactName: tds[1].querySelector("span").innerText,
+              position: tds[2].querySelector("span").innerText,
+              college: tds[3].querySelector("span").innerText,
+              contactPhone: tds[4].querySelector("span").innerText,
+            };
+          });
+        return rowsData;
+      });
+      return rows;
+    } catch (error) {
+      handleError(error);
+    }
   } else {
     return null;
   }
