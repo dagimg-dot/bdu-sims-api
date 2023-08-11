cf4cb240a824a5a6260c21ddeff56ca7eb4baadd
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-11 14:10:22 +0300

    better error handling in timeout and other errors in grades scraper

--- scraper/s_grades.js
+++ scraper/s_grades.js
@@ -1,5 +1,8 @@
 const browserPool = require("../utils/browser");
 const jwt = require("jsonwebtoken");
+const handleError = require("../utils/errorHandler");
+const User = require("../memory_db/user");
+const Pages = require("../utils/types");
 
 const courses = async (req) => {
   const token = req.headers.authorization.split(" ")[1];
@@ -9,36 +12,45 @@ const courses = async (req) => {
   const browser = await browserPool.getBrowserInstance(username);
   if (browser != null) {
     const page = await browser.newPage();
-    await page.goto("https://studentinfo.bdu.edu.et/MyGrades.aspx");
 
-    // find "There are no records available." in the html page
-    if (
-      (await page.$(
-        "#dnn_ctr398_ViewMyGrades_reportviewer14_grid2_ob_grid2BodyContainer > div.ob_gBICont > table > tbody > tr > td > div"
-      ).innerText) == "There are no records available."
-    ) {
-      return {
-        error: "There are no records available.",
-      };
-    }
+    const user = User.getUser(username);
 
-    const rows = await page.evaluate(() => {
-      const rowElements = document.querySelectorAll(
-        "#dnn_ctr398_ViewMyGrades_reportviewer14_grid2_ob_grid2BodyContainer > div.ob_gBICont > table > tbody > tr"
-      );
-      const rowsData = Array.from(rowElements).map((rowElement) => {
-        const tds = rowElement.querySelectorAll("td");
+    user.pages[Pages.GRADES].value = page;
+
+    try {
+      await page.goto("https://studentinfo.bdu.edu.et/MyGrades.aspx");
+
+      // find "There are no records available." in the html page
+      if (
+        (await page.$(
+          "#dnn_ctr398_ViewMyGrades_reportviewer14_grid2_ob_grid2BodyContainer > div.ob_gBICont > table > tbody > tr > td > div"
+        ).innerText) == "There are no records available."
+      ) {
         return {
-          courseCode: tds[0].querySelector("div > div").innerText,
-          courseTitle: tds[1].querySelector("div > div").innerText,
-          credit: tds[2].querySelector("div > div").innerText,
-          grade: tds[3].querySelector("div > div").innerText,
-          courseCategory: tds[4].querySelector("div > div").innerText,
+          error: "There are no records available.",
         };
+      }
+
+      const rows = await page.evaluate(() => {
+        const rowElements = document.querySelectorAll(
+          "#dnn_ctr398_ViewMyGrades_reportviewer14_grid2_ob_grid2BodyContainer > div.ob_gBICont > table > tbody > tr"
+        );
+        const rowsData = Array.from(rowElements).map((rowElement) => {
+          const tds = rowElement.querySelectorAll("td");
+          return {
+            courseCode: tds[0].querySelector("div > div").innerText,
+            courseTitle: tds[1].querySelector("div > div").innerText,
+            credit: tds[2].querySelector("div > div").innerText,
+            grade: tds[3].querySelector("div > div").innerText,
+            courseCategory: tds[4].querySelector("div > div").innerText,
+          };
+        });
+        return rowsData;
       });
-      return rowsData;
-    });
-    return rows;
+      return rows;
+    } catch (error) {
+      handleError(error);
+    }
   } else {
     return null;
   }
