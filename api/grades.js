1f50c00ac0b18f94e18b5cccb80773b4bdc907f1
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-13 22:13:52 +0300

    use the sendResult method in grades endpoint

--- api/grades.js
+++ api/grades.js
@@ -4,6 +4,7 @@ const s_grades = require("../scraper/s_grades");
 const User = require("../memory_db/user");
 const getUsername = require("../utils/usernameHandler");
 const Pages = require("../utils/types");
+const sendResult = require("../utils/sendResult");
 
 const courses = async (request, response) => {
   logger.info(
@@ -16,26 +17,11 @@ const courses = async (request, response) => {
 
   if (user.getGrades() !== null) {
     response.status(200).json({
-      grades: user.getGrades(),
+      [Pages.GRADES]: user.getGrades(),
     });
   } else {
     const result = await s_grades(request);
-    if (!response.headersSent) {
-      if (result !== null) {
-        user.setGrades(result);
-        response.status(200).json({
-          grades: result,
-        });
-      } else if (result === null) {
-        response.status(401).json({
-          message: "Unauthorized",
-        });
-      } else {
-        response.status(500).json({
-          message: "Internal Server Error",
-        });
-      }
-    }
+    sendResult(result, response, Pages.GRADES, () => user.setGrades);
   }
 };
 
