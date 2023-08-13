245b971c717d9c3a8634a2938bc911f10cd3b29a
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-13 22:14:05 +0300

    use the sendResult method in info endpoint

--- api/info.js
+++ api/info.js
@@ -4,6 +4,7 @@ const s_info = require("../scraper/s_info");
 const User = require("../memory_db/user");
 const getUsername = require("../utils/usernameHandler");
 const Pages = require("../utils/types");
+const sendResult = require("../utils/sendResult");
 
 const info = async (request, response) => {
   logger.info(
@@ -16,26 +17,11 @@ const info = async (request, response) => {
 
   if (user.getInfo() !== null) {
     response.status(200).json({
-      info: user.getInfo(),
+      [Pages.INFO]: user.getInfo(),
     });
   } else {
     const result = await s_info(request);
-    if (!response.headersSent) {
-      if (result !== null) {
-        user.setInfo(result);
-        response.status(200).json({
-          info: result,
-        });
-      } else if (result === null) {
-        response.status(401).json({
-          message: "Unauthorized",
-        });
-      } else {
-        response.status(500).json({
-          message: "Internal Server Error",
-        });
-      }
-    }
+    sendResult(result, response, Pages.INFO, () => user.setInfo);
   }
 };
 
