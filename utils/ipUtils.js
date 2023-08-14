2d17f79bad0362d28cd1b66f5024926ec5459a3c
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-14 10:22:33 +0300

    comment unused method

--- utils/ipUtils.js
+++ utils/ipUtils.js
@@ -8,23 +8,23 @@ const getClientIPAddress = (req) => {
   return ipAddress;
 };
 
-const getServerIPAddress = () => {
-  const networkInterfaces = os.networkInterfaces();
-  let ipAddress = "127.0.0.1"; // Default to localhost if IP address is not found
+// const getServerIPAddress = () => {
+//   const networkInterfaces = os.networkInterfaces();
+//   let ipAddress = "127.0.0.1"; // Default to localhost if IP address is not found
 
-  // Loop through all the network interfaces to find a non-internal IPv4 address
-  for (const interfaceName in networkInterfaces) {
-    const interfaces = networkInterfaces[interfaceName];
-    for (const iface of interfaces) {
-      if (!iface.internal && iface.family === "IPv4") {
-        ipAddress = iface.address;
-        break;
-      }
-    }
-  }
+//   // Loop through all the network interfaces to find a non-internal IPv4 address
+//   for (const interfaceName in networkInterfaces) {
+//     const interfaces = networkInterfaces[interfaceName];
+//     for (const iface of interfaces) {
+//       if (!iface.internal && iface.family === "IPv4") {
+//         ipAddress = iface.address;
+//         break;
+//       }
+//     }
+//   }
 
-  return ipAddress;
-};
+//   return ipAddress;
+// };
 
 module.exports = getClientIPAddress;
-module.exports = getServerIPAddress;
+// module.exports = getServerIPAddress;
