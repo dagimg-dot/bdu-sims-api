808ad40d39d03cfaf5b01870dd6dfaa943b8f0b8
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-14 10:25:49 +0300

    create Url type

--- utils/types.js
+++ utils/types.js
@@ -6,4 +6,14 @@ const Pages = {
     DETAIL_STATUS: 'detailStatus'
 }
 
-module.exports = Pages;
+const BASE_URL = 'https://studentinfo.bdu.edu.et' 
+
+const Url = {
+    LOGIN: `${BASE_URL}/login.aspx`,
+    INFO: `${BASE_URL}`,
+    COURSES: `${BASE_URL}/MyCourses.aspx`,
+    GRADES: `${BASE_URL}/MyGrades.aspx`,
+    STATUS: `${BASE_URL}/MyStatus.aspx`
+}
+
+module.exports = { Pages, Url };
