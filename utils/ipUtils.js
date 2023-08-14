350cf869b371926179b4a1cea4fdc2a75cbb6fdd
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-14 11:40:45 +0300

    fix ipaddress extractor  from the request method

--- utils/ipUtils.js
+++ utils/ipUtils.js
@@ -2,9 +2,10 @@ const os = require("os");
 
 const getClientIPAddress = (req) => {
   // Extract the IP address from the request object
-  const ipAddress =
+  let ipAddress =
     req.headers["x-forwarded-for"] || req.connection.remoteAddress;
 
+  ipAddress = ipAddress.split('f:')[1];
   return ipAddress;
 };
 
