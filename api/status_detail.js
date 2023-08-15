1c348fc884db3fe4041b0095cc461be785eec8fe
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-15 16:36:30 +0300

    set requested page

--- api/status_detail.js
+++ api/status_detail.js
@@ -1,6 +1,9 @@
 const logger = require("../logger/logger");
 const getClientIPAddress = require("../utils/ipUtils");
 const s_status_detail = require("../scraper/s_status_detail");
+const User = require("../memory_db/user");
+const getUsername = require("../utils/usernameHandler");
+const Pages = require("../utils/types").Pages;
 
 const courses = async (request, response) => {
   logger.info(
@@ -9,6 +12,10 @@ const courses = async (request, response) => {
 
   const year = request.params.year;
   const semester = request.params.semester;
+
+  const { username, isExpired } = getUsername(request);
+  const user = User.getUser(username);
+  user.setRequested(Pages.GENERAL_STATUS);
 
   if (isNaN(year) || isNaN(semester) || semester < 1 || semester >= 3) {
     response.status(400).json({
