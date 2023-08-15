cbdde48b4a452fb5a4849d015f489de5e93ffd0b
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-15 16:41:56 +0300

    use the send result method to send appropriate response

--- api/status_detail.js
+++ api/status_detail.js
@@ -3,9 +3,10 @@ const getClientIPAddress = require("../utils/ipUtils");
 const s_status_detail = require("../scraper/s_status_detail");
 const User = require("../memory_db/user");
 const getUsername = require("../utils/usernameHandler");
+const sendResult = require("../utils/sendResult");
 const Pages = require("../utils/types").Pages;
 
-const courses = async (request, response) => {
+const status_detail = async (request, response) => {
   logger.info(
     `Status Page requested from IP Address: ${getClientIPAddress(request)}`
   );
@@ -17,6 +18,8 @@ const courses = async (request, response) => {
   const user = User.getUser(username);
   user.setRequested(Pages.DETAIL_STATUS);
 
+  // TODO: Caching will be implemented in the future.
+
   if (isNaN(year) || isNaN(semester) || semester < 1 || semester >= 3) {
     response.status(400).json({
       error: {
@@ -25,20 +28,8 @@ const courses = async (request, response) => {
     });
   } else {
     const result = await s_status_detail(request);
-    if (result !== null) {
-      response.status(200).json({
-        courses: result,
-      });
-    } else if (result === null) {
-      response.status(401).json({
-        message: "Unauthorized",
-      });
-    } else {
-      response.status(500).json({
-        message: "Internal Server Error",
-      });
-    }
+    sendResult(result, response, Pages.DETAIL_STATUS, () => user.setDetailStatus)
   }
 };
 
-module.exports = courses;
+module.exports = status_detail;
