5fe4f5d126472f305d4dbabaced58e6770e884ce
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-18 22:18:01 +0300

    apply the course_qeury method to the course endpoint

--- api/courses.js
+++ api/courses.js
@@ -5,6 +5,7 @@ const User = require("../memory_db/user");
 const getUsername = require("../utils/usernameHandler");
 const Pages = require("../utils/types").Pages;
 const sendResult = require("../utils/sendResult");
+const courses_query = require("./with_query/courses_query");
 
 const courses = async (request, response) => {
   logger.info(
@@ -16,12 +17,24 @@ const courses = async (request, response) => {
   user.setRequested(Pages.COURSES);
 
   if (user.getCourses() !== null) {
-    response.status(200).json({
-      [Pages.COURSES]: user.getCourses(),
-    });
+    const result = user.getCourses();
+    if (request.query) {
+      courses_query(request, response, result);
+    } else {
+      response.status(200).json({
+        [Pages.COURSES]: result,
+      });
+    }
   } else {
     const result = await s_courses(request);
-    sendResult(result, response, Pages.COURSES, () => user.setCourses);
+    sendResult(
+      request,
+      response,
+      result,
+      Pages.COURSES,
+      () => user.setCourses,
+      courses_query
+    );
   }
 };
 
