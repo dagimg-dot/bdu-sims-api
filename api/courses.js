3e39d5c13208161401a2831bd13efd222a08f431
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-04-28 01:02:05 +0300

    refactor: remove code related to query

--- api/courses.js
+++ api/courses.js
@@ -4,8 +4,9 @@ const s_courses = require("../scraper/s_courses");
 const User = require("../memory_db/user");
 const getUsername = require("../utils/usernameHandler");
 const Pages = require("../utils/types").Pages;
-const sendResult = require("../utils/sendResult");
-const courses_query = require("./with_query/courses_query");
+const sendResult = require("../utils/utilityFunc").sendResult;
+const cacheResult = require("../utils/utilityFunc").cacheResult;
+const handleUnauthorized = require("../utils/errorHandler").handleUnauthorized;
 
 const courses = async (request, response) => {
   logger.info(
@@ -14,27 +15,26 @@ const courses = async (request, response) => {
 
   const username = getUsername(request).username;
   const user = User.getUser(username);
-  user.setRequested(Pages.COURSES);
+  if (!user) {
+    handleUnauthorized(response);
+    return;
+  } else {
+    user.setRequested(Pages.COURSES);
+  }
 
   if (user.getCourses() !== null) {
     const result = user.getCourses();
-    if (request.query) {
-      courses_query(request, response, result);
-    } else {
-      response.status(200).json({
-        [Pages.COURSES]: result,
-      });
-    }
+    response.status(200).json({
+      [Pages.COURSES]: result,
+    });
   } else {
-    const result = await s_courses(request);
-    sendResult(
-      request,
-      response,
-      result,
-      Pages.COURSES,
-      () => user.setCourses,
-      courses_query
-    );
+    try {
+      const result = await s_courses(request);
+      cacheResult(result, user.setCourses.bind(user));
+      sendResult(response, result, Pages.COURSES);
+    } catch (error) {
+      sendError(response, error);
+    }
   }
 };
 
